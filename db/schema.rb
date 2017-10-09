89cb99a8b5db7168ae1e603670e0cbef08675308
Pham Trong Tri <tri.phamtrong@toshiba-tsdv.com>
2017-10-09 09:49:46 +0700

    Init manage_topic branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171007092959) do
+ActiveRecord::Schema.define(version: 20171009024806) do
 
   create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "email", default: "", null: false
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(version: 20171007092959) do
     t.string "role"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "topics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
