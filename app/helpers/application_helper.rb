3884ee76724c088f5a42847604ccf5b4d61bfd05
Pham Trong Tri <trongtri160592@yahoo.com>
2017-10-30 00:46:11 +0700

    Add header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def approval_tag(lesson)
+    if lesson.approved == 1
+      '<span class="label label-success">Đã duyệt</span>'.html_safe
+    else
+      '<span class="label label-danger">Chưa duyệt</span>'.html_safe
+    end
+  end
+
+  def course_avatar(model)
+    avatar = model.avatar ? model.avatar : 'course_default_logo.jpg'
+    image_tag avatar, alt: model.name
+  end
 end
