852f28a7ac7d6285a82a4a020d2c5eb0a4af27af
Pham Trong Tri <tri.phamtrong@toshiba-tsdv.com>
2017-11-23 17:31:21 +0700

    Update lesson uploader

--- app/uploaders/scorm_uploader.rb
+++ app/uploaders/scorm_uploader.rb
@@ -37,7 +37,7 @@ class ScormUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-     %w(jpg jpeg gif png zip mp4 doc docx ppt pptx)
+     %w(jpg jpeg gif png zip mp4 doc docx ppt pptx mp3 swf html htm)
   end
 
   # Override the filename of the uploaded files:
